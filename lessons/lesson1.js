d1712ab964907bcedcefb6220d253dbe6e515519
Andrea Timaran <andreaestefania12@hotmail.com>
2022-03-03 14:09:26 -0500

    Execution context and stack

---
+++ lessons/lesson1.js
@@ -0,0 +1,68 @@
+// POW
+/*
+pow(2,2) = 4;
+pow(2,3) = 8;
+pow(2,4) = 16;
+*/
+
+// pow in iterative with for
+
+function powI(x,n){
+    let result = 1;
+    // multiply result by x n times in the loop
+    for (let i = 0; i < n ; i++){
+        result *= x;
+    }
+    return result;
+}
+
+// alert(powI(2,3));
+
+// pow with recursive thinking: simplify the task and call self:
+
+function powR(x,n){
+    if (n == 1) {
+        return x;
+    } else {
+        return x * powR(x,n-1);
+    }
+}
+// alert(powR(2,4));
+
+function powR(x,n){
+    return (n == 1) ? x : x * powR(x,n-1);
+}
+
+/* RECURSIVE SOLUTION IS USUALLY SHORTER THAN AN ITERATIVE ONE*/ 
+
+
+// RECURSIVE TRAVERSALS
+
+let company = {
+    sales: [{
+        name : 'Jhon',
+        salary : 1000
+    },
+    {
+        name : 'Alice',
+        salary : 1600
+    }],
+
+    development: {
+        sites: [{
+            
+            name : 'Peter',
+            salary : 2000
+        },
+        {
+            name : 'Alex',
+            salary : 1800
+        }],
+    
+    internals:[{
+            name : 'Jack',
+            salary : 1300
+        }],
+    }
+    
+}
