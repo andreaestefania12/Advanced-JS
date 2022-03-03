28caa5088a3cc4a7c0dc0aa29b2140a82e6b6b12
Andrea Timaran <andreaestefania12@hotmail.com>
2022-03-03 16:36:33 -0500

    Nested Functions

--- lessons/lesson3.js
+++ lessons/lesson3.js
@@ -41,3 +41,33 @@ for (let i = 0; i < 3; i++) {
 }
   
 // console.log(i); // Error, no such variable
+
+// NESTED FUNCTIONS
+
+// A function is called nested when it is created inside another function
+
+function sayHiBye(firstName, lastName) {
+
+    // helper nested function to use below
+    function getFullName() {
+      return firstName + " " + lastName;
+    }
+  
+    console.log( "Hello, " + getFullName() );
+    console.log( "Bye, " + getFullName() );
+}
+sayHiBye('Andrea','Timaran');
+
+
+function makeCounter() {
+    let count = 0;
+  
+    return function() {
+      return count++;
+    };
+}
+  
+let counter = makeCounter();  
+console.log( counter() ); // 0
+console.log( counter() ); // 1
+console.log( counter() ); // 2
