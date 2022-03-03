d4c91d0150b41c65914ff8eb5c39236f468de4e8
Andrea Timaran <andreaestefania12@hotmail.com>
2022-03-03 15:18:44 -0500

    Output a single linked list with loops and recursive solution

--- Tasks/Task1.js
+++ Tasks/Task1.js
@@ -61,3 +61,35 @@ function fib(n){
 }
 
 console.log(fib(77));
+
+/*Output a single linked list*/
+
+function printListLoop(list){
+    while(list){
+        console.log(list.value);
+        list = list.next;
+    }
+}
+
+function printListRecursive(list){    
+    console.log(list.value);
+    if(list.next){
+        printListRecursive(list.next);
+    }
+}
+
+list = {
+    value : 1,
+    next : {
+        value : 2,
+        next : {
+            value : 3,
+            next : {
+                value : 4, 
+                next : null
+            }
+        }
+    }
+};
+printListLoop(list);
+printListRecursive(list);
